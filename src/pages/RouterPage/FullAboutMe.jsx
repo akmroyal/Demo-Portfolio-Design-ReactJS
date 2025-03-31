343267a0753d91582570cde9bc8b299427a6ec44
akmroyal <ashutoshmaxxxx@gmail.com>
2025-03-31 22:05:15 +0530

    remove some lines

--- src/pages/RouterPage/FullAboutMe.jsx
+++ src/pages/RouterPage/FullAboutMe.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
-import HamburgerMenu from '../../components/short-component/Hamburger'
 
 const FullAboutMe = () => {
   return (
     <div className='bg-red-700 flex flex-col my-[20vw]'>
       helo
       <div>hd</div>
-      <div>eidf</div>
+      <div>eidf dks </div>
+      <div>f</div>
       {/* <HamburgerMenu /> */}
     </div>
   )
